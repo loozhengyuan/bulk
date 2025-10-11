ddab493989fd82eafe8bd6d8bbf92ddc4887d1aa
Loo Zheng Yuan <loozhengyuan@gmail.com>
2025-10-12 11:40:45 +1300

    chore: add linebreak for nicer output fmt

--- internal/engine/engine.go
+++ internal/engine/engine.go
@@ -33,7 +33,7 @@ func (e *Engine) Execute() error {
 	}
 
 	for _, repo := range repos {
-		fmt.Printf("Processing %s...", repo)
+		fmt.Printf("Processing %s...\n", repo)
 
 		remote := fmt.Sprintf("git@github.com:%s.git", repo)
 		r, err := NewRepository(e.p.ID, remote, e.force)
